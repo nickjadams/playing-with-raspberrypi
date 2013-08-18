8dcaf601ba5998a9bcced44602095d7053aa2399
nickjadams <nickjadams@gmail.com>
2013-08-18 07:46:02 +0100

    refactored code

--- temperaturesensor.py
+++ temperaturesensor.py
@@ -2,7 +2,18 @@
 
 from gpio.i2c.Adafruit_I2C import Adafruit_I2C
 
-bus = Adafruit_I2C(address=0x48)
-print str(bus.readS8(0)) + u"\u2103"
+#Constants
+TC74 = 0x48			# Address of the TC74 on the I2C bus
+REGISTER = 0x00	# The register where the temperature is read from
+UNITS = u'\u2103'
+
+bus = Adafruit_I2C(address=TC74)
+
+def readTemp():
+	return bus.readS8(REGISTER)
+
+if __name__=='__main__':
+	print str(readTemp()) + UNITS
+
 
 
