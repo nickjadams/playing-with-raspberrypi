591456e57d13e1504a3ce9aabe3d3568bbe60eda
nickjadams <nickjadams@gmail.com>
2013-08-25 00:13:00 +0100

    started to add code to handle interrupts

--- playing/expanders/i2c/io/MCP23017.py
+++ playing/expanders/i2c/io/MCP23017.py
@@ -5,11 +5,14 @@ from playing.pi.gpio.i2c.Adafruit_I2C import Adafruit_I2C
 import time
 
 class MCP23017Bank():
-   def __init__(self,name,direction=0xff,pullups=0x00,polarity=0x00,latch=0x00):
+   def __init__(self,name,direction=0xff,pullups=0x00,polarity=0x00,interuptsEnabled=0x00,interruptsDefaults=0x00,interruptsType=0xff,latch=0x00):
       self.name = name
       self.direction = direction
       self.pullups = pullups
       self.polarity = polarity
+      self.interuptsEnabled = interuptsEnabled
+      self.interruptsDefaults = interruptsDefaults
+      self.interruptsType = interruptsType
       self.latch = latch
 
    def printLatchValues(self):
@@ -43,16 +46,27 @@ class MCP23017Bank():
 class MCP23017():
 
    # Register Constants
-   IODIRA = 0x00     # Input or Output
-   IODIRB = 0x01     # Input or Output
-   IOPOLA = 0x02     # Invert polarity of input pins
-   IOPOLB = 0x03     # Invert polarity of input pins
-   GPPUA  = 0x0c     # Enable / disable pull-up resistors 
-   GPPUB  = 0x0d     # Enable / disable pull-up resistors 
-   GPIOA  = 0x12     # Read and write to the values in the bank
-   GPIOB  = 0x13     # Read and write to the values in the bank
-   OLATA  = 0x14     # Output latch 
-   OLATB  = 0x15     # Output latch
+   IODIRA   = 0x00     # Input or Output
+   IODIRB   = 0x01     # Input or Output
+   IOPOLA   = 0x02     # Invert polarity of input pins
+   IOPOLB   = 0x03     # Invert polarity of input pins
+   GPINTENA = 0x04     # Interrupt on change controls register (corrosponding DEFVAL and INTCON must also be configured)
+   GPINTENB = 0x05     # Interrupt on change controls register (corrosponding DEFVAL and INTCON must also be configured)
+   DEFVALA  = 0x06     # Default comapre register (interrupt will occur when there is a mismatch between this and the value of the pin) 
+   DEFVALB  = 0x07     # Default comapre register (interrupt will occur when there is a mismatch between this and the value of the pin)
+   INTCONA  = 0x08     # Interrupt control register (specifies how the interrupts work)
+   INTCONB  = 0x09     # Interrupt control register (specifies how the interrupts work)
+   IOCON    = 0x0a     # I/O expander configuration register 
+   GPPUA    = 0x0c     # Enable / disable pull-up resistors 
+   GPPUB    = 0x0d     # Enable / disable pull-up resistors 
+   INTFA    = 0x0e     # Interrupt flag register (shows which pin caused the interrupt)
+   INTFB    = 0xff     # Interrupt flag register (shows which pin caused the interrupt)
+   INTCAPA  = 0x10     # Interrupt capture register (the value at the time the interrupt occurred
+   INTCAPB  = 0x11     # Interrupt capture register (the value at the time the interrupt occurred
+   GPIOA    = 0x12     # Read and write to the pins in the bank
+   GPIOB    = 0x13     # Read and write to the pins in the bank
+   OLATA    = 0x14     # Output latch 
+   OLATB    = 0x15     # Output latch
 
 
    INPUT  = True
@@ -66,18 +80,23 @@ class MCP23017():
       self.i2c.write8(self.IODIRA, self.bankA.direction)
       self.i2c.write8(self.GPPUA, self.bankA.pullups)
       self.i2c.write8(self.IOPOLA, self.bankA.polarity)
+      self.i2c.write8(self.GPINTENA, self.bankA.interruptsEnabled)
+      self.12c.write8(self.DEFVALA, self.bankA.interruptsDefaults)
+      self.i2c.write(self.INTCONA, self.bankA.interruptsType)
       self.bankA.latch = self.i2c.readU8(self.OLATA)
       # Bank B
       self.i2c.write8(self.IODIRB, self.bankB.direction)
       self.i2c.write8(self.GPPUB, self.bankB.pullups)
       self.i2c.write8(self.IOPOLB, self.bankB.polarity)
+      self.i2c.write8(self.GPINTENB, self.bankB.interruptsEnabled)
+      self.12c.write8(self.DEFVALB, self.bankB.interruptsDefaults)
+      self.i2c.write(self.INTCONA, self.bankA.interruptsType)
       self.bankB.latch = self.i2c.readU8(self.OLATB)
 
 
    def output(self, bank, pin, value):
       assert 0 <= pin <= 7, 'Invalid pin number'
-      if bank != 'A':
-         assert bank == 'B', 'Bank can be either A or B nothing else'
+      assert bank in ('A', 'B'), 'Bank can be either A or B nothing else'
 
       if bank == 'A':
          if value:
